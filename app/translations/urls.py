e25187b4ddd3e8cd6205abdf3fa34dbfe84052a4
Andrew Ricci <andrewwricci@gmail.com>
2024-01-07 01:21:33 +0900

    URL修正

--- app/translations/urls.py
+++ app/translations/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "translations"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<uuid:original_text_id>/", views.detail, name="detail"),
