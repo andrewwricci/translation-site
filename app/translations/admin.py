400aac7b7b3913a2beac1025ea5b2e16993eedf3
Andrew Ricci <andrewwricci@gmail.com>
2024-02-12 00:31:53 +0900

    ignoring all pycache files

--- app/translations/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import User, Original_Text, Translated_Text
-
-admin.site.register(User)
-admin.site.register(Original_Text)
-admin.site.register(Translated_Text)
