6c3977306461fd1a4f18849204c86da1cfacb5b7
Andrew Ricci <andrewwricci@gmail.com>
2024-02-25 19:40:22 +0900

    emailでログインできるようにする

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from .views import SignUpView
+from . import views
 
 app_name = "accounts"
 
-urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-]
+urlpatterns = []
